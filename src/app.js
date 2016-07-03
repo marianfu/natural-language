a98273383d39fecf95e85b9a5d098cc69672191b
marianfu <marianfurriel@gmail.com>
2016-07-03 00:39:58 -0300

    some grammar def and parser generation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var _ = require('underscore');
 var pegjs = require('pegjs');
-var Parser = require('./grammar');
+var Parser = require('./parser');
 var Interpreter = require('./interpreter');
 var app = express();
 
@@ -14,22 +14,9 @@ app.listen(3000, function () {
     var interpreter = new Interpreter();
     console.log('Example app listening on port 3000!');
 
-    var str = Parser.parse('var a = 1');
-    var str2 = Parser.parse('var b = 2');
-    var str3 = Parser.parse('var c = a + b');
+    var str = Parser.parse('variable a es 1\n ' +
+        'variable b es 2.2\n' +
+        'variable c es "nombre"');
     console.log(str);
-
-    var variables = [];
-    variables.push(interpreter.interpret(str));
-    variables.push(interpreter.interpret(str2));
-    variables.push(interpreter.interpret(str3));
-    variables.push({name: 'c', value: eval(''+variables[0].value+''+variables[2].operator+''+variables[1].value)});
-
-    eval('var v = '+variables[0].value+''+variables[2].operator+''+variables[1].value);
-    variables.forEach(function (element) {
-        console.log(element);
-    });
-
-    console.log(v);
 });
 
