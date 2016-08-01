90ebee6c85028488114fd8e2eb0f565766a581ca
mariano.furriel <mariano.furriel@fdvsolutions.com>
2016-08-01 14:57:31 -0300

    test app grammar

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var _ = require('underscore');
 var pegjs = require('pegjs');
-var Parser = require('./parser');
+var Parser = require('./natural_parser');
 var JavascriptParser = require('./javascriptParser');
 var Interpreter = require('./interpreter');
 var escodegen = require('escodegen');
@@ -20,11 +20,11 @@ app.listen(3000, function () {
     var interpreter = new Interpreter();
     console.log('Example app listening on port 3000!');
 
-    var str = Parser.parse('variable a es 1.2');
+    var str = Parser.parse('var a es 1, b es 2, d es 4; var c es 3; c es 6; si (a igual 2 o c distinto 3) { } sino si(c == 2) { } sino { } metodo getEdad() { var e es 3; }');
 
     var str2 = JavascriptParser.parse('if(a == 2){}');
 
-    var result = escodegen.generate(str2);
+    var result = escodegen.generate(str);
 
     console.log(result);
     //Interpreter.interpret(str);
