1e899d0b52d77b97d438383c9ac43f31c82120ad
mariano.furriel <mariano.furriel@fdvsolutions.com>
2016-08-03 15:47:06 -0300

    more ...

--- src/app.js
+++ src/app.js
@@ -12,11 +12,14 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 
     var str = NaturalLanguage.parse('var a es 1, b es 2, d es 4; var c es 3; c es 6; si (a igual 2 y c distinto 3) { } sino si(c == 6) { console.log(getEdad(1,2))} sino { console.log("afa"); } getEdad(a, b) { devolver a+b; }');
+    var str2 = NaturalLanguage.parse('var array es [1,2,3]; por cada(a en array){ imprimir(array[a]); } imprimir(e) { console.log(e); }')
 
     var result = Escodegen.generate(str);
+    var result2 = Escodegen.generate(str2);
 
-    console.log(result);
+    console.log(result2);
 
-    eval(result);
+    //eval(result);
+    eval(result2);
 });
 
